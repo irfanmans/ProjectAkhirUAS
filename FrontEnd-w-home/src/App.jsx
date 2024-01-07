08023509277d0b6ec1aafa36b702315563066258
Veilind <142288106+moonlighttz@users.noreply.github.com>
2024-01-07 18:09:54 +0700

    Add files via upload

---
+++ FrontEnd-w-home/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./assets/components/UI/Navbar";
+import Home from "./assets/pages/home"
+import Login from "./assets/pages/Login";
+import Registrasi from "./assets/pages/Registrasi";
+
+function App() {
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Registrasi />} />
+                </Routes>
+            </Router>
+        </>
+    );
+}
+
+export default App;
---
+++ FrontEnd-w-home/src/assets/pages/Registrasi.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Button from "../components/Button/Button";
+
+export default function Registrasi() {
+    return (
+        <React.Fragment>
+            <article className="main-register">
+                <div className="wrapper-register">
+                    <div className="title-register">
+                        <h1>REGISTRASI</h1>
+                        <p>Silahkan registrasi akun anda</p>
+                    </div>
+                    <form className="main-form" action="">
+                        <div className="form-group">
+                            <label htmlFor="fullname">FullName</label>
+                            <input type="text" id="fullname" placeholder="Masukkan FullName" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" placeholder="Masukkan Email" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="noHandphone">Nomor Handphone</label>
+                            <input type="tel" id="noHandphone" placeholder="Masukkan No Handphone" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" placeholder="Masukkan Password" />
+                        </div>
+                    </form>
+                    <Button className="btn-register">Daftar Akun</Button>
+                    <div className="already-account">
+                        <span>
+                            Sudah memiliki akun? <NavLink to="/login">Login</NavLink>
+                        </span>
+                    </div>
+                </div>
+            </article>
+        </React.Fragment>
+    );
+}
