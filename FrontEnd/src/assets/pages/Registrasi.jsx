4b2332c58268b56ce74f3ec9531fb2ab84b3b771
Louis <lastwars52@gmail.com>
2024-01-07 19:31:41 +0700

    Fix issue from getting front end data

--- FrontEnd/src/assets/pages/Registrasi.jsx
+++ FrontEnd/src/assets/pages/Registrasi.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Button from "../components/Button/Button";
 import axios from "axios";
 
 export default function Registrasi() {
-    const RegisterUserAccount = function () {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [noHp, setNoHp] = useState("");
+    const [password, setPassword] = useState("");
+
+    const RegisterUserAccount = (event) => {
+        event.preventDefault();
+
+        // const username = document.getElementById("fullname").value
+        // const email = document.getElementById("email").value
+        // const noHp = document.getElementById("noHandphone").value
+        // const password = document.getElementById("password").value
         console.log("AAAAAAAAAAAAAAAAAAAAA")
-        const token = localStorage.getItem('jwtToken');
-
-        const username = document.getElementById("fullname").value
-        const email = document.getElementById("email").value
-        const noHp = document.getElementById("noHandphone").value
-        const password = document.getElementById("password").value
-
+        // const token = localStorage.getItem('jwtToken');
         const User = {
             name: username,
             password: password,
@@ -22,7 +28,7 @@ export default function Registrasi() {
 
         axios.post("http://localhost:3000/signup", User)
             .then(response => {
-                console.log('Server response:', response.data);
+                console.log('Server response:');
             })
             .catch(error => {
                 console.error('Error during registration:', error);
@@ -40,22 +46,22 @@ export default function Registrasi() {
                     <form className="main-form" action="">
                         <div className="form-group">
                             <label htmlFor="fullname">FullName</label>
-                            <input type="text" id="fullname" placeholder="Masukkan FullName" />
+                            <input type="text" id="fullname" placeholder="Masukkan FullName" onChange={(e) => setUsername(e.target.value)} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
-                            <input type="email" id="email" placeholder="Masukkan Email" />
+                            <input type="email" id="email" placeholder="Masukkan Email" onChange={(e) => setEmail(e.target.value)} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="noHandphone">Nomor Handphone</label>
-                            <input type="tel" id="noHandphone" placeholder="Masukkan No Handphone" />
+                            <input type="tel" id="noHandphone" placeholder="Masukkan No Handphone" onChange={(e) => setNoHp(e.target.value)} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
-                            <input type="password" id="password" placeholder="Masukkan Password" />
+                            <input type="password" id="password" placeholder="Masukkan Password" onChange={(e) => setPassword(e.target.value)} />
                         </div>
                     </form>
-                    <Button type="button" className="btn-register" onClick={RegisterUserAccount}>Daftar Akun</Button>
+                    <button type="button" className="btn-register" onClick={RegisterUserAccount}>Daftar Akun</button>
 
                     <div className="already-account">
                         <span>
