830f2385d75ba867415c6466fc0e986060a355a8
Veilind <142288106+moonlighttz@users.noreply.github.com>
2024-01-14 17:19:07 +0700

    Add files via upload

---
+++ FrontEnd/src/assets/components/UI/Maps.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Maps(){
+    return(
+        <>
+            <div className="wrapper-maps">
+                <h3>Our Location</h3>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7476.906968804243!2d115.21500362774498!3d-8.828636779185032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd25cc0e01a2dfb%3A0x486d1b655b87ed9c!2sThe%20Apurva%20Kempinski%20Bali!5e0!3m2!1sen!2sid!4v1705213182506!5m2!1sen!2sid"></iframe>
+            </div>
+        </>
+    )
+}
