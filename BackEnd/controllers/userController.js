4d21e6a2362cb367ac983f0facdba160e17b40e4
louis <louis@origaming.com>
2024-01-16 12:37:13 +0700

    Revert "melakukan perbaikan userController dan models serta perbaikan halaman register"
    This reverts commit 5aa83daf37eb612c8d933502e6a429338e3a55a6.

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,11 +1,12 @@
 const User = require("../models/userModels.js");
 const bcrypt = require("bcrypt");
-const jwtController = require("./jwtController.js");
+const jwt = require("jsonwebtoken");
+const jwtController = require("./jwtController.js")
 
 const getUsers = async (req, res) => {
   try {
     const response = await User.findAll();
-    console.log(response);
+    console.log(response)
     res.status(200).json(response);
   } catch (err) {
     console.error(err);
@@ -13,6 +14,7 @@ const getUsers = async (req, res) => {
 };
 
 const userLogin = async (req, res) => {
+
   try {
     const user = await User.findOne({
       where: {
@@ -30,23 +32,24 @@ const userLogin = async (req, res) => {
       user.password
     );
     if (!isValidPassowrd) {
-      console.log("password is not valid");
+      console.log("password is not valid")
       res.status(401);
       return;
     }
 
-    const token = jwtController.generateToken(user);
-    const expiredTime = jwtController.expiredDay * 24 * 60 * 60 * 1000;
+    const token = jwtController.generateToken(user)
+    const expiredTime = jwtController.expiredDay * 24 * 60 * 60 * 1000
     res.cookie("access_token", token, {
       httpOnly: false,
-      sameSite: "None",
+      sameSite: 'None',
       secure: false,
-      path: "/",
-      credentials: "include", // Ensure credentials are included in the response
+      path: '/',
+      credentials: 'include',  // Ensure credentials are included in the response
       expiresIn: expiredTime,
     });
-    console.log(token);
+    console.log(token)
     res.json({ token });
+
   } catch (err) {
     console.error(err);
     res.status(400);
@@ -54,57 +57,31 @@ const userLogin = async (req, res) => {
 };
 
 const userRegister = async (req, res) => {
+
   try {
-    if (!req.body.username) {
-      return res.status(400).json({
-        error: "Username is required",
-      });
-    }
-    if (!req.body.email) {
-      return res.status(400).json({
-        error: "Email is required",
-      });
-    }
-    if (!req.body.password) {
-      return res.status(400).json({
-        error: "Password is required",
-      });
-    }
-    if (!req.body.phone_number) {
-      return res.status(400).json({
-        error: "Phone number is required",
-      });
-    }
-
     const existingUser = await User.findOne({
-      where: {
-        username: req.body.username,
-      },
+      where: { username: req.body.name }
     });
-    if (existingUser) {
-      return res.status(400).json({
-        error: "Username already exists",
-      });
-    }
 
+    if (existingUser) {
+      return res.status(400).json({ msg: "Username already exists" });
+    }
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const newUser = await User.create({
-      username: req.body.username,
+    await User.create({
+      username: req.body.name,
       password: hashedPassword,
       email: req.body.email,
-      phone_number: req.body.phone_number,
+      phone_number: req.body.no_hp
     });
-    res.status(201).json({
-      msg: "User created successfully",
-    });
-  } catch (error) {
+    res.status(201).json({ msg: "User Created" });
+  } catch (err) {
     console.error(err);
     res.status(500).json({ msg: "Error Creating User" });
   }
 };
 
 module.exports = {
-  getUsers: getUsers,
-  userRegister: userRegister,
-  userLogin: userLogin,
-};
+  "getUsers": getUsers,
+  "userRegister": userRegister,
+  "userLogin": userLogin
+}
--- BackEnd/models/userModels.js
+++ BackEnd/models/userModels.js
@@ -4,41 +4,30 @@ const db = require("../config/dataBase.js");
 const User = db.define(
   "User",
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-
     username: {
       type: DataTypes.STRING,
+      primaryKey: true,
       allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
+    email:{
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      },
+      allowNull : false
     },
     phone_number: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+      type : DataTypes.STRING,
+      allowNull : false
+    }
   },
   {
     freezeTableName: true,
   }
 );
 
-module.exports = User;
+module.exports =  User;
 
 (async () => {
   await db.sync();
