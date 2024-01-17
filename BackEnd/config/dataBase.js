3dce7adc649c951ec1b60014201f1628e547981b
Louis <lastwars52@gmail.com>
2024-01-17 18:54:53 +0700

    Revert "Merge branch 'fix/issue-on-login' of https://github.com/irfanmans/ProjectAkhirUAS into fix/issue-on-login"
    This reverts commit ebb9d19b8f16963e9c40c54bbf79f45037feab90, reversing
    changes made to 8d46d38ad78877d79a7f3080d957f6a20ef4948a.

--- BackEnd/config/dataBase.js
+++ BackEnd/config/dataBase.js
@@ -1,16 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("project_test", "root", "", {
-  host: "localhost",
+const db = new Sequelize("defaultdb", "avnadmin", "AVNS_02Mv067oP0AwjGGh-vc", {
+  host: "project-akhir-uas-project-akhir-uas.a.aivencloud.com",
+  port: 10834,
   dialect: "mysql",
 });
 
 db.authenticate()
-  .then(() => {
-    console.log("Successfully connect to database");
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
-  });
+.then(() => {
+  console.log("Successfully connect to database");
+})
+.catch((error) => {
+  console.error("Unable to connect to the database:", error);
+});
 
-module.exports = db;
+module.exports = db
