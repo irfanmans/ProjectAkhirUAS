7711c0db2c8037e32900506bc564fc9f849c08ab
el07duha <elroyduha@gmail.com>
2024-01-19 20:23:40 +0700

    remove alert pesan kesalahan error saat registrasi

--- FrontEnd/src/assets/pages/Registrasi.jsx
+++ FrontEnd/src/assets/pages/Registrasi.jsx
@@ -4,87 +4,102 @@ import Button from "../components/Button/Button";
 import axios from "axios";
 
 export default function Registrasi() {
-    const navigate = useNavigate()
-    const RegisterUserAccount = (event) => {
-        event.preventDefault();
+  const navigate = useNavigate();
+  const RegisterUserAccount = (event) => {
+    event.preventDefault();
 
-        const username = document.getElementById("fullname").value;
-        const email = document.getElementById("email").value;
-        const noHp = document.getElementById("noHandphone").value;
-        const password = document.getElementById("password").value;
+    const username = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const noHp = document.getElementById("noHandphone").value;
+    const password = document.getElementById("password").value;
 
-        if (!email.includes("@") || !email.includes(".com")) {
-            alert("Please check the email");
-            return;
-        }
-        const numericRegex = /^[0-9]+$/;
-        const isValidPhoneNumber = numericRegex.test(noHp);
+    if (!email.includes("@") || !email.includes(".com")) {
+      alert("Please check the email");
+      return;
+    }
+    const numericRegex = /^[0-9]+$/;
+    const isValidPhoneNumber = numericRegex.test(noHp);
 
-        if (!isValidPhoneNumber) {
-            alert("Please check the phone number");
-            return;
-        }
+    if (!isValidPhoneNumber) {
+      alert("Please check the phone number");
+      return;
+    }
 
-        // const token = localStorage.getItem('jwtToken');
-        const User = {
-            name: username,
-            password: password,
-            no_hp: noHp,
-            email: email,
-        };
-
-        console.log(User)
-
-        axios
-            .post("http://localhost:3000/signup", User)
-            .then((response) => {
-                localStorage.setItem("access_token", response.data.token)
-                console.log("Server response:");
-                navigate('/addbook')
-            })
-            .catch((error) => {
-                console.error("Error during registration:", error);
-                alert("Silahkan periksa data anda, atau cari username yang lain")
-            });
+    // const token = localStorage.getItem('jwtToken');
+    const User = {
+      name: username,
+      password: password,
+      no_hp: noHp,
+      email: email,
     };
 
-    return (
-        <React.Fragment>
-            <article className="main-register">
-                <div className="wrapper-register">
-                    <div className="title-register">
-                        <h1>REGISTRASI</h1>
-                        <p>Silahkan registrasi akun anda</p>
-                    </div>
-                    <form className="main-form" action="">
-                        <div className="form-group">
-                            <label htmlFor="fullname">FullName</label>
-                            <input type="text" id="fullname" placeholder="Masukkan FullName" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" placeholder="Masukkan Email" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="noHandphone">Nomor Handphone</label>
-                            <input type="tel" id="noHandphone" placeholder="Masukkan No Handphone" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" placeholder="Masukkan Password" />
-                        </div>
-                    </form>
-                    <button type="button" className="btn-register" onClick={RegisterUserAccount}>
-                        Daftar Akun
-                    </button>
+    console.log(User);
 
-                    <div className="already-account">
-                        <span>
-                            Sudah memiliki akun? <NavLink to="/login">Login</NavLink>
-                        </span>
-                    </div>
-                </div>
-            </article>
-        </React.Fragment>
-    );
+    axios
+      .post("http://localhost:3000/signup", User)
+      .then((response) => {
+        localStorage.setItem("access_token", response.data.token);
+        console.log("Server response:");
+        navigate("/addbook");
+      })
+      .catch((error) => {
+        console.error("Error during registration:", error);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <article className="main-register">
+        <div className="wrapper-register">
+          <div className="title-register">
+            <h1>REGISTRASI</h1>
+            <p>Silahkan registrasi akun anda</p>
+          </div>
+          <form className="main-form" action="">
+            <div className="form-group">
+              <label htmlFor="fullname">FullName</label>
+              <input
+                type="text"
+                id="fullname"
+                placeholder="Masukkan FullName"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="Masukkan Email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="noHandphone">Nomor Handphone</label>
+              <input
+                type="tel"
+                id="noHandphone"
+                placeholder="Masukkan No Handphone"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Masukkan Password"
+              />
+            </div>
+          </form>
+          <button
+            type="button"
+            className="btn-register"
+            onClick={RegisterUserAccount}
+          >
+            Daftar Akun
+          </button>
+
+          <div className="already-account">
+            <span>
+              Sudah memiliki akun? <NavLink to="/login">Login</NavLink>
+            </span>
+          </div>
+        </div>
+      </article>
+    </React.Fragment>
+  );
 }
